05a7179add0ea8659572e822b5f2038c84d17b16
Ritij Jain <ritij@ritijjain.com>
2020-08-04 03:48:08 +0530

    ran collectstatic

--- ClassGC/settings.py
+++ ClassGC/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY =  os.environ["CLASSGC_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['classgc.herokuapp.com']
+ALLOWED_HOSTS = ['connectpie.herokuapp.com']
 
 
 # Application definition
